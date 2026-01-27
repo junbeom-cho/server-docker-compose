4cd4fba6e3de7ed612399f8cf692f4ca0d342be4
junbeom-cho <dev.junbeom@gmail.com>
2026-01-27 13:56:32 +0900

    reset git

--- postgresql/sql/authentik-db.sql
+++
@@ -1,5 +0,0 @@
-CREATE USER authentik_user WITH PASSWORD '${PG_AUTHENTIK_USER_PASSWORD}';
-
-CREATE DATABASE authentik_db OWNER authentik_user;
-
-GRANT ALL PRIVILEGES ON DATABASE authentik_db TO authentik_user;
